4267a520d52d0aaa4f3bba5c5e6c06543450435a
sergey12313 <sergey12313@gmail.com>
2023-09-18 21:16:32 +0300

    feat: fetching feed data #5

---
+++ src/app/shared/modules/feed/store/selectors.ts
@@ -0,0 +1,20 @@
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+import {FeedStateInterface} from './feedState.interface';
+
+export const feedFeatureSelector =
+  createFeatureSelector<FeedStateInterface>('feed');
+
+export const isLoadingSelector = createSelector(
+  feedFeatureSelector,
+  (state) => state.isLoading
+);
+
+export const feedDataSelector = createSelector(
+  feedFeatureSelector,
+  (state) => state.data
+);
+
+export const feedErrorSelector = createSelector(
+  feedFeatureSelector,
+  (state) => state.error
+);
--- src/app/shared/types/appState.interface.ts
+++ src/app/shared/types/appState.interface.ts
@@ -1,5 +1,7 @@
 import {AuthStateInterface} from 'src/app/auth/types/authState.interface';
+import {FeedStateInterface} from '../modules/feed/store/feedState.interface';
 
 export interface AppStateInterface {
   auth: AuthStateInterface;
+  feed: FeedStateInterface;
 }
